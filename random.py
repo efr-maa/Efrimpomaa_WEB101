33a5f586a2c7a98af85f2336cd561e990f89b336
Elizabeth Frimpomaa <elizabethfrimpomaa887@gmail.com>
2026-06-27 23:19:37 -0400

    File content updated

---
+++ random.py
@@ -0,0 +1,22 @@
+def greet_user(name):
+  print("Hello " + name)
+
+name = "Michael"
+greet_user("Micheal")
+
+print()
+def difference(a, b):
+  return a - b
+
+diff = difference(8,3)
+print(diff)
+
+print()
+#understand: input =list / output = list edge caese= empty/ list 
+#logic= a fuction creates and returns the a particular list ans whose length is 2 times the length
+#of a given interge list 
+
+
+def concatenat_list(num):
+  list_ans = ""
+
